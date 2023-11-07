5fb2359b233e10905d6caa068e54a4732c3e2cd4
rigra <kth5352@gmail.com>
2023-11-07 11:56:59 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20190325/poker/MainActivity.kt
@@ -0,0 +1,77 @@
+package kr.ac.kumoh.ce.s20190325.poker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kr.ac.kumoh.ce.s20190325.poker.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var main: ActivityMainBinding
+    private lateinit var model: CardDealerViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        main = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(main.root)
+
+        model = ViewModelProvider(this)[CardDealerViewModel::class.java]
+        model.cards.observe(this, Observer {
+            val res = IntArray(5)
+            for (i in it.indices) {
+                res[i] = resources.getIdentifier(
+                    getCardName(it[i]),
+                    "drawable",
+                    packageName
+                )
+            }
+
+            main.card1.setImageResource(res[0])
+//            main.card2.setImageResource(res[1])
+//            main.card3.setImageResource(res[2])
+//            main.card4.setImageResource(res[3])
+//            main.card5.setImageResource(res[4])
+        })
+
+        main.btnShuffle.setOnClickListener {
+            model.shuffle()
+        }
+    }
+
+    private fun getCardName(c: Int) : String {
+        // val에서 var로 변경
+        var shape = when (c / 13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+
+        val number = when (c % 13) {
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> {
+                shape = shape.plus("2")
+                "jack"
+            }
+            11 -> {
+                shape = shape.plus("2")
+                "queen"
+            }
+            12 -> {
+                shape = shape.plus("2")
+                "king"
+            }
+            else -> "error"
+        }
+
+        // 이 방법이 더 나을 듯
+//        if (c % 13 in 10..12)
+//            return "c_${number}_of_${shape}2"
+//        else
+//            return "c_${number}_of_${shape}"
+
+        return "c_${number}_of_${shape}"
+    }
+}
